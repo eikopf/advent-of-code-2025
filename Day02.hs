2191da7928b1b5e27a09c9c084cc3882fd65fe96
Oliver Wooding <oliverwooding@icloud.com>
2025-12-02 14:26:20 +0100

    day2: part 2 solution

--- Day02.hs
+++ Day02.hs
@@ -6,13 +6,15 @@ import Control.Monad (join)
 import Data.Ix.Enum (range)
 import Data.List.Split (splitWhen)
 
+-- | Parses the puzzle input into a sequence of (start, end) pairs.
 parse :: String -> [(Int, Int)]
 parse = pairs . map read . splitWhen (`elem` "-,") . filter (/= '\n')
 
+-- | Converts the longest even prefix of the input into a list of adjacent pairs. Note that this
+-- will drop the last element of the list if the list is finite and of odd length.
 pairs :: [a] -> [(a, a)]
-pairs [] = []
-pairs [x] = error "odd number of elements"
 pairs (x : y : zs) = (x, y) : pairs zs
+pairs _ = []
 
 solvePart1 :: [(Int, Int)] -> Int
 solvePart1 = sum . filter isInvalidPart1 . join . map range
@@ -27,9 +29,34 @@ isInvalidPart1 i =
       (prefix, suffix) = splitAt (len `div` 2) (show i)
    in prefix == suffix
 
+solvePart2 :: [(Int, Int)] -> Int
+solvePart2 = sum . filter isInvalidPart2 . join . map range
+
+-- | Returns `True` iff the given integer is an invalid ID in the second part of the problem.
+--
+-- Here an integer is invalid if there is some prefix of its decimal digits such that repeating the
+-- prefix at least twice results in the number itself. A small optimisation here is to fail when the
+-- prefix exceeds half the length of the entire digit string, since it must occur at least twice.
+isInvalidPart2 :: Int -> Bool
+isInvalidPart2 i =
+  let digits = show i
+      len = length digits
+      prefix = take (len `div` 2) digits
+   in go prefix digits len
+  where
+    go :: String -> String -> Int -> Bool
+    go [] _ _ = False
+    go prefix digits len =
+      (digits == take len (cycle prefix) && (len `rem` length prefix) == 0)
+        || go (take (length prefix - 1) prefix) digits len
+
 -- | Returns the number of decimal digits in the input.
 decimalDigits :: Int -> Int
 decimalDigits = length . show -- this is probably stupidly inefficient
 
 main :: IO ()
-main = interact (show . solvePart1 . parse)
+main = interact $ \s ->
+  let input = parse s
+      part1 = solvePart1 input
+      part2 = solvePart2 input
+   in "part 1: " ++ show part1 ++ "\npart 2: " ++ show part2
