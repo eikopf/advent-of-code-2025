1c93f9d86ead6d320ab51b9035674e112b07c9ef
Oliver Wooding <oliverwooding@icloud.com>
2025-12-03 13:19:23 +0100

    day3: cleaned up code for both parts

--- Day03.hs
+++ Day03.hs
@@ -1,21 +1,21 @@
-{-# OPTIONS_GHC -Wno-typed-holes #-}
-
 module Day03 where
 
-import Data.Bifunctor (bimap)
 import Data.Char (digitToInt)
-import Data.List (singleton, subsequences)
 
+-- | Parses the input into a list of digit lists.
 parse :: String -> [[Int]]
 parse = map (map digitToInt) . lines
 
+-- | Solves part 1 by finding the sum of the largest subsequences of length 2.
 solvePart1 :: [[Int]] -> Int
 solvePart1 = sum . map maxPair
   where
+    -- Drops the last element of a finite list (identity on infinite lists).
     dropLast :: [a] -> [a]
     dropLast [x] = []
     dropLast (x : xs) = x : dropLast xs
 
+    -- Returns the value of the largest subsequence of length 2.
     maxPair :: [Int] -> Int
     maxPair digits =
       let prefixMax = maximum (dropLast digits)
@@ -23,9 +23,11 @@ solvePart1 = sum . map maxPair
           suffixMax = maximum suffix
        in prefixMax * 10 + suffixMax
 
+-- | Solves part 2 by finding the sum of the largest subsequences of length 12.
 solvePart2 :: [[Int]] -> Int
 solvePart2 = sum . map (maxKSubsequence 12)
   where
+    -- Finds the largest subsequence of length k.
     maxKSubsequence :: Int -> [Int] -> Int
     maxKSubsequence 0 _ = 0
     maxKSubsequence 1 digits = maximum digits
@@ -35,6 +37,7 @@ solvePart2 = sum . map (maxKSubsequence 12)
           suffix = drop 1 (dropWhile (/= maxPrefix) digits)
        in maxPrefix * (10 ^ (k - 1)) + maxKSubsequence (k - 1) suffix
 
+    -- Drops the last k elements of a finite list.
     dropLastK :: Int -> [a] -> [a]
     dropLastK k = reverse . drop k . reverse
 
