685395b70ed68324a4e515bf9ea6a1bc377fd328
Oliver Wooding <oliverwooding@icloud.com>
2025-12-04 12:32:26 +0100

    day4: completed part 1

---
+++ Day04.hs
@@ -0,0 +1,52 @@
+module Day04 where
+
+import Data.Array (Array, bounds, indices, listArray, (!))
+import Data.Ix (inRange)
+
+data Cell
+  = Occupied
+  | Empty
+  deriving (Show, Eq)
+
+charToCell :: Char -> Cell
+charToCell '.' = Empty
+charToCell '@' = Occupied
+charToCell _ = error "invalid cell"
+
+parse :: String -> Array (Int, Int) Cell
+parse s =
+  let rows = lines s
+      rowLen = length (head rows)
+      colLen = length rows
+      elems = map charToCell $ concat rows
+   in listArray ((1, 1), (rowLen, colLen)) elems
+
+solvePart1 :: Array (Int, Int) Cell -> Int
+solvePart1 grid = length $ filter (< 4) $ map (countOccupiedNeighbors grid) $ filter ((==) Occupied . (!) grid) $ indices grid
+  where
+    countOccupiedNeighbors :: Array (Int, Int) Cell -> (Int, Int) -> Int
+    countOccupiedNeighbors grid i = length $ filter ((==) Occupied . (!) grid) $ neighbors grid i
+
+    neighbors :: Array (Int, Int) Cell -> (Int, Int) -> [(Int, Int)]
+    neighbors grid i = filter (inRange (bounds grid)) (offsets <*> [i])
+
+    offsets :: [(Int, Int) -> (Int, Int)]
+    offsets =
+      map
+        (\(x, y) (a, b) -> (a + x, b + y))
+        [ (-1, -1),
+          (-1, 0),
+          (-1, 1),
+          (0, -1),
+          (0, 1),
+          (1, -1),
+          (1, 0),
+          (1, 1)
+        ]
+
+main :: IO ()
+main = interact $ \s ->
+  let input = parse s
+      part1 = solvePart1 input
+      part2 = () -- solvePart2 input
+   in "part 1: " ++ show part1 ++ "\npart 2: " ++ show part2 ++ "\n"
