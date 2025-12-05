d5864ab4323fc1f18c97a5ea5368e65c55e0eadb
Oliver Wooding <oliverwooding@icloud.com>
2025-12-05 11:20:56 +0100

    day5: completed part 2

--- Day05.hs
+++ Day05.hs
@@ -3,8 +3,9 @@
 module Day05 where
 
 import Data.Bifunctor (bimap)
-import Data.Ix (inRange)
-import Data.List (findIndex)
+import Data.Function (fix)
+import Data.Ix (inRange, rangeSize)
+import Data.List (findIndex, foldl')
 
 data Db = Db [(Int, Int)] [Int]
   deriving (Show)
@@ -18,9 +19,30 @@ parse = uncurry Db . bimap (map readRange) (map read . drop 1) . break null . li
 solvePart1 :: Db -> Int
 solvePart1 (Db rs is) = length $ filter (flip any rs . flip inRange) is
 
+solvePart2 :: Db -> Int
+solvePart2 (Db rs _) = sum . map rangeSize $ go rs
+  where
+    go :: [(Int, Int)] -> [(Int, Int)]
+    go rs =
+      let rs' = foldl' upsertRange [] rs
+       in if rs == rs' then rs else go rs'
+
+    upsertRange :: [(Int, Int)] -> (Int, Int) -> [(Int, Int)]
+    upsertRange rs r' | rangeSize r' == 0 = rs
+    upsertRange [] r' = [r']
+    upsertRange (r : rs) r' = case unionMaybe r r' of
+      Nothing -> r : upsertRange rs r'
+      Just r'' -> r'' : rs
+
+    unionMaybe :: (Int, Int) -> (Int, Int) -> Maybe (Int, Int)
+    unionMaybe lhs@(l1, l2) rhs@(r1, r2) = if overlap lhs rhs then Just (min l1 r1, max l2 r2) else Nothing
+
+    overlap :: (Int, Int) -> (Int, Int) -> Bool
+    overlap lhs@(l1, l2) rhs@(r1, r2) = inRange lhs r1 || inRange rhs l1
+
 main :: IO ()
 main = interact $ \s ->
   let input = parse s
       part1 = solvePart1 input
-      part2 = () -- solvePart2 input
+      part2 = solvePart2 input
    in "part 1: " ++ show part1 ++ "\npart 2: " ++ show part2 ++ "\n"
