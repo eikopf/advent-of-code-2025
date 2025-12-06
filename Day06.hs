1ab6a165a88ffcf22e2829acbc47d6690fc74e8b
Oliver Wooding <oliverwooding@icloud.com>
2025-12-06 12:12:42 +0100

    day6: completed part 2

--- Day06.hs
+++ Day06.hs
@@ -23,31 +23,38 @@ identity :: (Num a) => Op -> a
 identity Add = 0
 identity Mul = 1
 
-parse :: String -> Maybe [Problem]
-parse s = do
+processOpLine :: String -> [(Op, Int)]
+processOpLine [] = []
+processOpLine ('+' : cs) = let (spaces, tail) = span (== ' ') cs in (Add, length spaces) : processOpLine tail
+processOpLine ('*' : cs) = let (spaces, tail) = span (== ' ') cs in (Mul, length spaces) : processOpLine tail
+processOpLine _ = error "invalid operator"
+
+takeLengths :: [Int] -> [a] -> [[a]]
+takeLengths [] xs = []
+takeLengths [l] xs = [take (l + 1) xs]
+takeLengths (l : ls) xs = take l xs : takeLengths ls (drop (l + 1) xs)
+
+parsePart1 :: String -> Maybe [Problem]
+parsePart1 s = do
   (rows, lastRow) <- unsnoc $ lines s
   let ops = processOpLine lastRow
   let elems = transpose $ map (takeLengths (map snd ops)) rows
   let problems = zipWith Problem (map fst ops) (map (map read) elems)
   return problems
-  where
-    processOpLine :: String -> [(Op, Int)]
-    processOpLine [] = []
-    processOpLine ('+' : cs) = let (spaces, tail) = span (== ' ') cs in (Add, length spaces) : processOpLine tail
-    processOpLine ('*' : cs) = let (spaces, tail) = span (== ' ') cs in (Mul, length spaces) : processOpLine tail
-    processOpLine _ = error "invalid operator"
 
-    takeLengths :: [Int] -> [a] -> [[a]]
-    takeLengths [] xs = []
-    takeLengths [l] xs = [take (l + 1) xs]
-    takeLengths (l : ls) xs = take l xs : takeLengths ls (drop (l + 1) xs)
+parsePart2 :: String -> Maybe [Problem]
+parsePart2 s = do
+  (rows, lastRow) <- unsnoc $ lines s
+  let ops = processOpLine lastRow
+  let elems = map transpose $ transpose $ map (takeLengths (map snd ops)) rows
+  let problems = zipWith Problem (map fst ops) (map (map read) elems)
+  return problems
 
-solvePart1 :: [Problem] -> Int
-solvePart1 = sum . map (\(Problem op is) -> foldl' (runOp op) (identity op) is)
+solve :: [Problem] -> Int
+solve = sum . map (\(Problem op is) -> foldl' (runOp op) (identity op) is)
 
 main :: IO ()
 main = interact $ \s ->
-  let input = parse s
-      part1 = solvePart1 <$> input
-      part2 = () -- solvePart2 <$> input
+  let part1 = solve <$> parsePart1 s
+      part2 = solve <$> parsePart2 s
    in "part 1: " ++ show part1 ++ "\npart 2: " ++ show part2 ++ "\n"
