65b86dd0d95a1d935df3ac10ed4ac43a100f8c42
Oliver Wooding <oliverwooding@icloud.com>
2025-12-07 12:00:34 +0100

    day7: cleaned up both parts

--- Day07.hs
+++ Day07.hs
@@ -8,54 +8,49 @@ import Data.Ix (inRange)
 import Data.List qualified as List
 
 data Manifold = Manifold
-  { rows :: Int,
-    cols :: Int,
-    startCol :: Int,
+  { startCol :: Int,
     splitters :: [(Int, Int)]
   }
-  deriving (Show)
 
 parse :: String -> Maybe Manifold
 parse s = do
-  let rawRows = lines s
-  (firstRow, splitterRows) <- List.uncons rawRows
+  let rows = lines s
+  (firstRow, splitterRows) <- List.uncons rows
   startCol <- List.elemIndex 'S' firstRow
   let splitters = parseSplitters (zip splitterRows [1 ..])
-  let rows = length rawRows
-  let cols = length firstRow
-  return Manifold {rows, cols, startCol, splitters}
+  return Manifold {startCol, splitters}
   where
     parseSplitters :: [(String, Int)] -> [(Int, Int)]
     parseSplitters = List.concatMap (\(s, row) -> map (row,) (List.elemIndices '^' s))
 
 solvePart1 :: Manifold -> Int
-solvePart1 (Manifold {cols, startCol, splitters}) = go (IntSet.singleton startCol) (0, cols - 1) splitters
+solvePart1 (Manifold {startCol, splitters}) = go (IntSet.singleton startCol) splitters
   where
-    go :: IntSet -> (Int, Int) -> [(Int, Int)] -> Int
-    go cols _ [] = 0
-    go cols range ((_, col) : ss) =
-      let (cols', split) = if IntSet.member col cols then (insertSplitCols cols range col, True) else (cols, False)
-       in (if split then 1 else 0) + go cols' range ss
+    go :: IntSet -> [(Int, Int)] -> Int
+    go cols [] = 0
+    go cols ((_, col) : ss) =
+      let (cols', split) = if IntSet.member col cols then (insertSplitCols cols col, True) else (cols, False)
+       in (if split then 1 else 0) + go cols' ss
 
-    insertSplitCols :: IntSet -> (Int, Int) -> Int -> IntSet
-    insertSplitCols cols range col =
-      let cols' = if inRange range (col - 1) then IntSet.insert (col - 1) cols else cols
-          cols'' = if inRange range (col + 1) then IntSet.insert (col + 1) cols' else cols'
+    insertSplitCols :: IntSet -> Int -> IntSet
+    insertSplitCols cols col =
+      let cols' = IntSet.insert (col - 1) cols
+          cols'' = IntSet.insert (col + 1) cols'
        in IntSet.delete col cols''
 
 solvePart2 :: Manifold -> Int
-solvePart2 (Manifold {cols, startCol, splitters}) = sum . IntMap.elems $ go (IntMap.singleton startCol 1) (0, cols - 1) splitters
+solvePart2 (Manifold {startCol, splitters}) = sum . IntMap.elems $ go (IntMap.singleton startCol 1) splitters
   where
-    go :: IntMap Int -> (Int, Int) -> [(Int, Int)] -> IntMap Int
-    go timelines _ [] = timelines
-    go timelines range ((_, col) : ss) = go (updateTimelines timelines range col) range ss
+    go :: IntMap Int -> [(Int, Int)] -> IntMap Int
+    go timelines [] = timelines
+    go timelines ((_, col) : ss) = go (updateTimelines timelines col) ss
 
-    updateTimelines :: IntMap Int -> (Int, Int) -> Int -> IntMap Int
-    updateTimelines timelines range col = case IntMap.lookup col timelines of
+    updateTimelines :: IntMap Int -> Int -> IntMap Int
+    updateTimelines timelines col = case IntMap.lookup col timelines of
       Nothing -> timelines
       Just n ->
-        let timelines' = if inRange range (col - 1) then IntMap.alter (updateOrSet (+ n) n) (col - 1) timelines else timelines
-            timelines'' = if inRange range (col + 1) then IntMap.alter (updateOrSet (+ n) n) (col + 1) timelines' else timelines'
+        let timelines' = IntMap.alter (updateOrSet (+ n) n) (col - 1) timelines
+            timelines'' = IntMap.alter (updateOrSet (+ n) n) (col + 1) timelines'
          in IntMap.delete col timelines''
 
     updateOrSet :: (a -> a) -> a -> Maybe a -> Maybe a
