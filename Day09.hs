d673fd3a724bd25d8b6d2864a6a46997e224d106
Oliver Wooding <oliverwooding@icloud.com>
2025-12-09 17:18:55 +0100

    day9: incomplete implementation of part 2

--- Day09.hs
+++ Day09.hs
@@ -1,9 +1,15 @@
+{-# OPTIONS_GHC -Wno-typed-holes #-}
+
 module Day09 where
 
+import Data.Bifoldable (biall)
 import Data.Bifunctor (bimap)
+import Data.Ix (Ix, range)
 import Data.List (tails)
 import Data.List.NonEmpty (NonEmpty ((:|)), nonEmpty)
 import Data.Maybe (mapMaybe)
+import Data.Set (Set)
+import Data.Set qualified as Set
 
 parse :: String -> [(Int, Int)]
 parse = map (bimap read (read . drop 1) . break (== ',')) . lines
@@ -20,9 +26,40 @@ pairs =
 solvePart1 :: [(Int, Int)] -> Int
 solvePart1 = maximum . map (uncurry area) . pairs
 
+solvePart2 :: [(Int, Int)] -> Int
+solvePart2 ps =
+  let edgeSet = buildPolygon ps
+      vertexSet = Set.fromList ps
+      width = maximum (map fst ps)
+      p = inside vertexSet edgeSet width
+   in maximum . map (uncurry area) . filter (biall p p . otherCorners) . pairs $ ps
+  where
+    otherCorners :: ((a, a), (a, a)) -> ((a, a), (a, a))
+    otherCorners ((a, b), (c, d)) = ((a, d), (c, b))
+
+    -- this works, but takes forever to solve part 2
+    -- maybe there's a faster way to check this value? could i build a quadtree?
+
+    inside :: Set (Int, Int) -> Set (Int, Int) -> Int -> (Int, Int) -> Bool
+    inside vertexSet edgeSet width p@(px, py) =
+      let vertexHits = length $ filter (`Set.member` vertexSet) $ map (,py) [px .. width]
+          edgeHits = length $ filter (`Set.member` edgeSet) $ map (,py) [px .. width]
+       in Set.member p edgeSet
+            || odd vertexHits
+            || (vertexHits == 0 && odd edgeHits)
+
+    buildPolygon :: [(Int, Int)] -> Set (Int, Int)
+    buildPolygon = Set.fromList . concatMap range' . adjacentPairs
+
+    range' :: (Ix a) => (a, a) -> [a]
+    range' (a, b) = if a < b then range (a, b) else range (b, a)
+
+    adjacentPairs :: [a] -> [(a, a)]
+    adjacentPairs xs = zip xs (drop 1 (cycle xs))
+
 main :: IO ()
 main = interact $ \s ->
   let input = parse s
       part1 = solvePart1 input
-      part2 = () -- solvePart2 input
+      part2 = solvePart2 input
    in "part 1: " ++ show part1 ++ "\npart 2: " ++ show part2 ++ "\n"
