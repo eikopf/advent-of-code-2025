d711014e879b3399448fbfca9b9335c1dc03b359
Oliver Wooding <oliverwooding@icloud.com>
2025-12-11 15:42:08 +0100

    day11: incomplete implementation of part 2

--- Day11.hs
+++ Day11.hs
@@ -36,9 +36,23 @@ solvePart1 graph = go graph Set.empty ('y', 'o', 'u')
       successors <- edges graph !? start
       return . sum . mapMaybe (go graph visitedNodes') . filter (not . flip Set.member visitedNodes') $ successors
 
+solvePart2 :: Graph Node -> Maybe Int
+solvePart2 graph = go graph Set.empty (False, False) ('s', 'v', 'r')
+  where
+    go :: Graph Node -> Set Node -> (Bool, Bool) -> Node -> Maybe Int
+    go graph visitedNodes state ('o', 'u', 't')
+      | state == (True, True) = return 1
+      | otherwise = return 0
+    go graph visitedNodes (dacSeen, fftSeen) start = do
+      let visitedNodes' = Set.insert start visitedNodes
+      let dacSeen' = dacSeen || start == ('d', 'a', 'c')
+      let fftSeen' = fftSeen || start == ('f', 'f', 't')
+      successors <- edges graph !? start
+      return . sum . mapMaybe (go graph visitedNodes' (dacSeen', fftSeen')) . filter (not . flip Set.member visitedNodes') $ successors
+
 main :: IO ()
 main = interact $ \s ->
   let input = parse s
       part1 = solvePart1 <$> input
-      part2 = () -- solvePart2 input
+      part2 = solvePart2 <$> input
    in "part 1: " ++ show part1 ++ "\npart 2: " ++ show part2 ++ "\n"
